1ea1866a33a78cb3e396d28687b3fb56f0d9f3d3
Ben <benjaminlagrone@gmail.com>
2015-07-01 17:27:25 -0500

    fixing ajax calls to new paths

--- js/controllers.js
+++ js/controllers.js
@@ -472,6 +472,7 @@ angular.module('MDAndersonMobile.controllers', ['ui.bootstrap', 'geolocation', '
                 }
             } else {
                 console.log("getCHildren1");
+                console.log($scope.forms[a]);
                 console.log($scope.forms[a].id);
                 console.log($scope.formOptionsModels[a].modelData.id);
                 getChildren($scope.forms[a].id + $scope.formOptionsModels[a].modelData.id, a)
